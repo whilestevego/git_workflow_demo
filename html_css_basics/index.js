2bc958f3661dbf18309127642fda0c98075c89a6
Steve Godin <steve.god@gmail.com>
2017-11-20 12:54:58 -0800

    Add notes for HTML & CSS: Scripts, Box Model & Flex

---
+++ html_css_basics/index.js
@@ -0,0 +1,10 @@
+console.log(`I'm JavaScript inside a file! 📁📁📁`);
+
+// Single and double quotes both create strings and there no differences
+// between each.
+'I\'m bob'
+""
+
+// Strings written with backticks are named Template Strings. It allows to
+// run JavaScript inside of the backticks (String interpolation).
+console.log(`Hello, my name is ${"Steve ".repeat(5)}!`);
